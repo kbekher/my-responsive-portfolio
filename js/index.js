5c4ad7fe3e5fa91ba4628a10cbaea49e1f13f0cc
Kristina <107880739+NinjaGexly@users.noreply.github.com>
2022-10-18 10:38:49 +0300

    Working on certificate change on click
    Created an array of objects (certificates)
    Added event listeners to arrows

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
+function viewNextCertificate(event) {
+  event.preventDefault();
+}
+
+function viewPrevCertificate(event) {
+  event.preventDefault();
+}
+
 let certificates = [
   {
     name: "SheCodes Basics",
     testimonial:
-      "The workshop was absolutely amazing! The way of presenting the material is clear and simple, the support in Slack is very fast. I really enjoyed learning how to code and create my own pages. This workshop is a great introduction to learning programming, it makes me want to dive into coding. Thanks a lot to Matthieu Delac and the SheCodes team! Looking forward to further learning!",
+      "SheCodes Basics is a great introduction to learning programming. I really enjoyed learning how to code and create my own HTML pages. The workshop made me want to dive into coding!",
     certificateLink:
       "https://www.shecodes.io/certificates/dc4e6b642e7a27d77f67af0d5d72de68",
     imagePath: "/images/certificates/shecodes-basics.png",
@@ -10,7 +18,7 @@ let certificates = [
   {
     name: "SheCodes Plus",
     testimonial:
-      "This has been an amazing course full of different challenges I loved to go through. The course is well explained, so you'll never feel like you're lost. It was so exciting to work on my own project and I am so happy and satisfied with what I've created. SheCodes gave me confidence and inspired me to start my carrier in tech. I highly recommend it and looking forward to continue learning to code with SheCodes.",
+      "SheCodes Plus is an amazing adventure I would highly recommend. It was a challenging, but well structured and fun course. I learned how to use API key and host pages, as well as developed my HTML, CSS and JS skills. ",
     certificateLink:
       "https://www.shecodes.io/certificates/2cffcd760c2126c9b4f7ef4966b4f05e",
     imagePath: "/images/certificates/shecodes-plus.png",
@@ -18,13 +26,20 @@ let certificates = [
   {
     name: "SheCodes Responsive",
     testimonial:
-      "SheCodes Responsive is a great course that helped me to learn how to build responsive websites that look good on different devices. It was very exciting to work on my own portfolio website and I am very happy with the result. My HTML and CSS skills have improved greatly and I am looking forward to continue developing my skills.",
+      "SheCodes Responsive is a great course that helped me to learn how to build responsive websites that look good on different devices. My HTML and CSS skills have improved greatly and I am looking forward to continue developing my skills.",
     certificateLink:
       "https://www.shecodes.io/certificates/ddde496309404c40a1d4cca4650e679f",
     imagePath: "/images/certificates/shecodes-responsive.png",
   },
 ];
 
+let arrowRight = document.querySelector("#arrow-right");
+arrowRight.addEventListener("click", viewNextCertificate);
+
+let arrowLeft = document.querySelector("#arrow-left");
+arrowLeft.addEventListener("click", viewPrevCertificate);
+
+/*Contact Form*/
 var form = document.getElementById("my-form");
 
 async function handleSubmit(event) {
