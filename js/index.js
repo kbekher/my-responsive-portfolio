13fb9fee3dcf5f5d0236413edca4c0279cc910b8
Kristina <107880739+NinjaGexly@users.noreply.github.com>
2023-01-16 00:38:34 +0300

    Created functions for certificate change
    Certificates change on click of left and right arrows

--- js/index.js
+++ js/index.js
@@ -1,34 +1,88 @@
+function showNextCertificate() {
+  if (index != certificates.length - 1) {
+    index++;
+  }
+
+  if (index === certificates.length - 1) {
+    arrowRight.classList.remove("arrow-branding");
+    arrowRight.classList.add("arrow-branding-outline");
+  }
+
+  arrowLeft.classList.remove("arrow-branding-outline");
+  arrowLeft.classList.add("arrow-branding");
+
+  let certificateTestimonial = document.querySelector("#testimonial");
+  certificateTestimonial.innerHTML = certificates[index].testimonial;
+
+  let certificateLink = document.querySelector("#certificate-link");
+  certificateLink.setAttribute("href", certificates[index].link);
+
+  let certificateImage = document.querySelector("#certificate-image");
+  certificateImage.setAttribute("src", certificates[index].imagePath);
+  certificateImage.setAttribute(
+    "alt",
+    `${certificates[index].name} certificate image`
+  );
+}
+
+function showPrevCertificate() {
+  if (index != 0) {
+    index = index - 1;
+  }
+  if (index === 0) {
+    arrowLeft.classList.remove("arrow-branding");
+    arrowLeft.classList.add("arrow-branding-outline");
+  }
+
+  arrowRight.classList.remove("arrow-branding-outline");
+  arrowRight.classList.add("arrow-branding");
+
+  let certificateTestimonial = document.querySelector("#testimonial");
+  certificateTestimonial.innerHTML = certificates[index].testimonial;
+
+  let certificateLink = document.querySelector("#certificate-link");
+  certificateLink.setAttribute("href", certificates[index].link);
+
+  let certificateImage = document.querySelector("#certificate-image");
+  certificateImage.setAttribute("src", certificates[index].imagePath);
+  certificateImage.setAttribute(
+    "alt",
+    `${certificates[index].name} certificate image`
+  );
+}
+
 function viewNextCertificate(event) {
   event.preventDefault();
+  showNextCertificate();
 }
 
 function viewPrevCertificate(event) {
   event.preventDefault();
+  showPrevCertificate();
 }
+
+let index = 0;
 
 let certificates = [
   {
     name: "SheCodes Basics",
     testimonial:
       "SheCodes Basics is a great introduction to learning programming. I really enjoyed learning how to code and create my own HTML pages. The workshop made me want to dive into coding!",
-    certificateLink:
-      "https://www.shecodes.io/certificates/dc4e6b642e7a27d77f67af0d5d72de68",
+    link: "https://www.shecodes.io/certificates/dc4e6b642e7a27d77f67af0d5d72de68",
     imagePath: "/images/certificates/shecodes-basics.png",
   },
   {
     name: "SheCodes Plus",
     testimonial:
       "SheCodes Plus is an amazing adventure I would highly recommend. It was a challenging, but well structured and fun course. I learned how to use API key and host pages, as well as developed my HTML, CSS and JS skills. ",
-    certificateLink:
-      "https://www.shecodes.io/certificates/2cffcd760c2126c9b4f7ef4966b4f05e",
+    link: "https://www.shecodes.io/certificates/2cffcd760c2126c9b4f7ef4966b4f05e",
     imagePath: "/images/certificates/shecodes-plus.png",
   },
   {
     name: "SheCodes Responsive",
     testimonial:
       "SheCodes Responsive is a great course that helped me to learn how to build responsive websites that look good on different devices. My HTML and CSS skills have improved greatly and I am looking forward to continue developing my skills.",
-    certificateLink:
-      "https://www.shecodes.io/certificates/ddde496309404c40a1d4cca4650e679f",
+    link: "https://www.shecodes.io/certificates/ddde496309404c40a1d4cca4650e679f",
     imagePath: "/images/certificates/shecodes-responsive.png",
   },
 ];
@@ -39,7 +93,6 @@ arrowRight.addEventListener("click", viewNextCertificate);
 let arrowLeft = document.querySelector("#arrow-left");
 arrowLeft.addEventListener("click", viewPrevCertificate);
 
-/*Contact Form*/
 var form = document.getElementById("my-form");
 
 async function handleSubmit(event) {
